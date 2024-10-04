20e7d40bb6f0e6b63c94a968e310befbbc2e65e1
mende273 <mende_273@hotmail.com>
2024-10-04 02:18:10 +0200

    include AppNavigation in App

--- composeApp/src/commonMain/kotlin/mende273/quoteskmp/App.kt
+++ composeApp/src/commonMain/kotlin/mende273/quoteskmp/App.kt
@@ -1,28 +1,97 @@
 package mende273.quoteskmp
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import mende273.quoteskmp.navigation.AppNavigation
+import mende273.quoteskmp.navigation.Screen
+import mende273.quoteskmp.ui.common.component.bottombar.BottomNavigationBar
+import mende273.quoteskmp.ui.common.component.bottombar.BottomNavigationItem
+import mende273.quoteskmp.ui.theme.QuotesKmpTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        Column(
-            Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
+    // val greeting = remember { Greeting().greet() }
+    // Text("Compose: $greeting")
 
-            val greeting = remember { Greeting().greet() }
-            Text("Compose: $greeting")
+    val navController = rememberNavController()
+
+    val bottomNavigationItems = enumValues<BottomNavigationItem>()
+
+    var isNavigationBarVisible by remember { mutableStateOf(false) }
+
+    val currentNavigationBarDestination = navController
+        .currentBackStackEntryAsState()
+        .value
+        .currentDestinationFromNavigationBar(
+            enumValues<BottomNavigationItem>()
+                .map { it.route }
+        )
+
+    isNavigationBarVisible = currentNavigationBarDestination != null
+
+    QuotesKmpTheme {
+        Scaffold(
+            content = {
+                AppNavigation(
+                    navHostController = navController
+                )
+            },
+            bottomBar = {
+                AnimatedVisibility(
+                    visible = isNavigationBarVisible,
+                    enter = slideInVertically(initialOffsetY = { it }),
+                    exit = slideOutVertically(targetOffsetY = { it })
+                ) {
+                    BottomNavigationBar(
+                        currentNavigationBarDestination,
+                        bottomNavigationItems,
+                        onNavigateToScreen = { navController.singleTopNavigate(it) }
+                    )
+                }
+            }
+        )
+    }
+}
+
+private fun NavBackStackEntry?.currentDestinationFromNavigationBar(
+    navigationBarItems: List<Screen>
+): Screen? {
+    var currentDestination: Screen? = null
+
+    navigationBarItems.forEach { item ->
+        if (this?.destination?.hierarchy?.any {
+                it.hasRoute(item::class)
+            } == true
+        ) {
+            currentDestination = item
         }
+    }
+
+    return currentDestination
+}
+
+private fun NavHostController.singleTopNavigate(screen: Screen) {
+    navigate(screen) {
+        popUpTo(graph.findStartDestination().id) {
+            saveState = true
+        }
+        launchSingleTop = true
+        restoreState = true
     }
 }
